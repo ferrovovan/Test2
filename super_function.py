16d2bc8d0e9f28baa60acc12557035eb489d507a
ferrovovan <ferrovovan@mail.ru>
2022-01-31 21:44:12 +0500

    add data

---
+++ super_function.py
@@ -0,0 +1,45 @@
+import requests
+
+
+def true_params(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    # ширина и высота
+    envelope = toponym['boundedBy']['Envelope']
+
+    def convert_fl_num(num):
+        num = num.split('.')
+        if int(num[1][0]) < 5:
+            return int(num[0])
+        return int(num[0]) + 1
+
+    x, y = tuple(map(convert_fl_num, envelope['lowerCorner'].split()))
+    delta = str((y - x) / 3600)
+
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+    return map_params
